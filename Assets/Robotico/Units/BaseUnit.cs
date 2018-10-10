852d69e66c88e3153a4d47bdca5f3980f83b0940
David Niggli <>
2018-10-10 20:34:51 +0200

    added Unity project

---
+++ Assets/Robotico/Units/BaseUnit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class BaseUnit : MonoBehaviour
+{   
+    // Update is called once per frame
+    protected virtual void Update()
+	{
+        transform.rotation = Quaternion.Euler(0f, 90f * Time.unscaledTime, 0f);
+		transform.position = new Vector3(Mathf.Cos(Time.unscaledTime), Mathf.Sin(Time.unscaledTime), 0.5f * Mathf.Cos(Time.unscaledTime));
+	}
+}
